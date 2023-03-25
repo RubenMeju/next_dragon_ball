b89d7fdd03c82b9de0e9ad29edab0c8f3a399b20
RubenMeju <rubenmeju@outlook.es>
2023-03-25 08:52:56 +0100

    selected NavItems

--- src/app/components/navBar/navItems/NavItems.jsx
+++ src/app/components/navBar/navItems/NavItems.jsx
@@ -1,13 +1,21 @@
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { pages } from '../const_nav'
 import stylesNavItems from './NavItems.module.css'
 
 export const NavItems = () => {
+  const pathname = usePathname()
   return (
     <ul className={stylesNavItems.ul}>
       {pages.map(({ name, href }) => (
         <li key={name} className={stylesNavItems.li}>
-          <Link href={href} className={stylesNavItems.link}>
+          <Link
+            href={href}
+            className={
+              (stylesNavItems.link,
+              pathname === href ? stylesNavItems.selected : null)
+            }
+          >
             {name}
           </Link>
         </li>
