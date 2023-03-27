57a004bf63cd1ab581ce743f161afb36bd3dc73e
RubenMeju <rubenmeju@outlook.es>
2023-03-27 22:24:05 +0200

    limpieza home

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
 import styles from './page.module.css'
-import PruebaAnimated from './components/Prueba/PruebaAnimated'
-import { getCharacters } from './services/getCharacters'
 
 export default async function Home() {
-  const characters = await getCharacters()
   return (
     <main className={styles.main}>
       <h1>Home page</h1>
-      <PruebaAnimated data={characters} />
     </main>
   )
 }
