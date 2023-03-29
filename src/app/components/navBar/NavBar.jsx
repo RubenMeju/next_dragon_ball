3133cefb742744efcaa2a6b355bbe222ef80f1fc
RubenMeju <rubenmeju@outlook.es>
2023-03-29 22:50:47 +0200

    Probando estilos

--- src/app/components/navBar/NavBar.jsx
+++ src/app/components/navBar/NavBar.jsx
@@ -1,12 +1,32 @@
+'use client'
 import Image from 'next/image'
+import { useCallback, useEffect, useState } from 'react'
 import Logo from '../../../../public/letras_dragonball.png'
 import MenuItems from './menuItems/MenuItems'
 
 import StylesNavBar from './NavBar.module.css'
 
 export default function NavBar() {
+  const [scrollY, setScrollY] = useState()
+  const onScroll = useCallback((event) => {
+    const { pageYOffset, scrollY } = window
+    console.log('yOffset', pageYOffset, 'scrollY', scrollY)
+    setScrollY(window.pageYOffset)
+  }, [])
+
+  useEffect(() => {
+    // add eventlistener to window
+    window.addEventListener('scroll', onScroll, { passive: true })
+    // remove event on unmount to prevent a memory leak with the cleanup
+    return () => {
+      window.removeEventListener('scroll', onScroll, { passive: true })
+    }
+  }, [])
   return (
-    <nav className={StylesNavBar.navBar}>
+    <nav
+      className={`
+        ${StylesNavBar.navBar} ${scrollY >= '40' && StylesNavBar.navFixed}`}
+    >
       <div className={StylesNavBar.cont_image}>
         <Image
           fill={true}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
 import styles from './page.module.css'
 
 export default async function Home() {
-  return (
-    <main className={styles.main}>
-      <h1 className="title">Home page</h1>
-    </main>
-  )
+  return <main className={styles.main}></main>
 }
