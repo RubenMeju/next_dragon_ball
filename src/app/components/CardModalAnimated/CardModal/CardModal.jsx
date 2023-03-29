064e4d47d172d50517235dc4a16d3724bcce6d68
RubenMeju <rubenmeju@outlook.es>
2023-03-29 17:25:44 +0200

    styles modal responsive

--- src/app/components/CardModalAnimated/CardModal/CardModal.jsx
+++ src/app/components/CardModalAnimated/CardModal/CardModal.jsx
@@ -25,59 +25,52 @@ export function CardModal() {
             exit={{ opacity: 0 }}
             className={StylesCardModal.contModal}
           >
-            <div className={StylesCardModal.contCard}>
-              <div className={StylesCardModal.cardImage}>
-                <Image
-                  fill={true}
-                  src={datos.image}
-                  className={StylesCardModal.image}
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
-                  loading="lazy"
-                  alt="Logo Dragon Ball"
-                />
-              </div>
+            <div className={StylesCardModal.modalHeader}>
+              <p className={StylesCardModal.modalName}>{datos.name}</p>
+              <motion.button
+                type="button"
+                className={StylesCardModal.btnExit}
+                whileHover={{
+                  scale: 1.1
+                }}
+                whileTap={{ scale: 0.9 }}
+                onClick={() => setSelectedId(null)}
+              ></motion.button>
+            </div>
 
-              <div className={StylesCardModal.cardBody}>
-                <p className={StylesCardModal.cardName}>{datos.name}</p>
+            <div className={StylesCardModal.cardImage}>
+              <Image
+                fill={true}
+                src={datos.image}
+                className={StylesCardModal.image}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+                loading="lazy"
+                alt="Logo Dragon Ball"
+              />
+            </div>
 
-                <p className={StylesCardModal.info}>{datos.description}</p>
+            <div className={StylesCardModal.modalBody}>
+              <p className={StylesCardModal.label}>Descripción</p>
+              <p className={StylesCardModal.info}>{datos.description}</p>
 
-                <div className={StylesCardModal.cardDetails}>
-                  <div className={StylesCardModal.groupDetails}>
-                    <Image
-                      src={svgPlanet}
-                      alt="planet"
-                      width={48}
-                      height={48}
-                    />
-                    <p className={StylesCardModal.info}>
-                      {datos.planet && datos.planet.name}
-                    </p>
-                  </div>
-                  <div className={StylesCardModal.groupDetails}>
-                    <Image
-                      src={svgSkeleton}
-                      alt="skeleton"
-                      width={48}
-                      height={48}
-                    />
-                    <p className={StylesCardModal.info}>{datos.species}</p>
-                  </div>
+              <div className={StylesCardModal.cardDetails}>
+                <div className={StylesCardModal.groupDetails}>
+                  <Image src={svgPlanet} alt="planet" width={48} height={48} />
+                  <p className={StylesCardModal.info}>
+                    {datos.planet && datos.planet.name}
+                  </p>
+                </div>
+                <div className={StylesCardModal.groupDetails}>
+                  <Image
+                    src={svgSkeleton}
+                    alt="skeleton"
+                    width={48}
+                    height={48}
+                  />
+                  <p className={StylesCardModal.info}>{datos.species}</p>
                 </div>
               </div>
             </div>
-
-            <motion.button
-              type="button"
-              className={StylesCardModal.button}
-              whileHover={{
-                scale: 1.1
-              }}
-              whileTap={{ scale: 0.9 }}
-              onClick={() => setSelectedId(null)}
-            >
-              Cerrar
-            </motion.button>
           </motion.div>
         </motion.div>
       )}
